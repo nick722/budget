b5d411b2dd307399e5ed99d98415ef6354e4ed58
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-10-27 10:08:27 +0300

    add styles for balance pane

--- src/components/transactions/balance-pane/balance-pane.js
+++ src/components/transactions/balance-pane/balance-pane.js
@@ -5,7 +5,10 @@ import "./balance-pane.scss";
 const BalancePane = () => {
   return (
     <div className="balance-pane">
-      <div className="">Overall balance 5100</div>
+      <div className="balance-banner">
+        <span>Overall balance </span>
+        <span className="balance-sum">5100</span>
+      </div>
     </div>
   );
 };
