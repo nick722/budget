a2de646c1f1e58ec8aa0fe2f40e556a8898c1e48
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-10-27 16:29:25 +0300

    add month-banner

---
+++ src/components/transactions/nav-pane/month-banner/month-banner.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./month-banner.scss";
+
+const MonthBanner = () => {
+  return <div className="month-banner">October 2019</div>;
+};
+
+export default MonthBanner;
--- src/components/transactions/nav-pane/nave-pane.js
+++ src/components/transactions/nav-pane/nave-pane.js
@@ -1,9 +1,14 @@
 import React from "react";
+import MonthBanner from "./month-banner/month-banner";
 
 import "./nav-pane.scss";
 
 const NavPane = () => {
-  return <div className="nave-pane">Nav Pane</div>;
+  return (
+    <div className="nave-pane">
+      <MonthBanner />
+    </div>
+  );
 };
 
 export default NavPane;
