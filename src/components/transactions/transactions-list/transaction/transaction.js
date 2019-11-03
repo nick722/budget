28254645a1fb432be03c642c89ec03104d914056
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-11-03 12:01:14 +0300

    add colors to transactions

--- src/components/transactions/transactions-list/transaction/transaction.js
+++ src/components/transactions/transactions-list/transaction/transaction.js
@@ -1,8 +1,18 @@
 import React from "react";
 
-const Transaction = ({ transaction: { amount, description } }) => {
+const Transaction = ({ transaction: { amount, description, type } }) => {
+  const setBackgroundColor = transactionType => {
+    if (transactionType === "Income") return "#dcf0d9";
+    if (transactionType === "Expense") return "#f8dede";
+  };
+
+  const style = {
+    color: "blue",
+    backgroundColor: setBackgroundColor(type)
+  };
+
   return (
-    <tr className="transaction-entry">
+    <tr className="transaction" style={style}>
       <td>{description}</td>
       <td>{amount}</td>
     </tr>
