8e558ec1a71c9395696732420127ec9f211eee58
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-11-25 22:26:29 +0300

    fix unit test for App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Transactions from "./components/transactions/transactions";
-import "./App.scss";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+
+import "./App.scss";
 
 function App() {
   return (
