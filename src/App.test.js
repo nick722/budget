a9a73a943595773ae7c0e3d299ab535714463d28
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-12-16 22:51:39 +0300

    WIP add tests for nav routes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import { MemoryRouter } from "react-router-dom";
 import App from "./App";
+import Transactions from "./components/transactions/transactions";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("App", () => {
+  it("should render without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("should render Home page WHEN route is /", () => {
+    const component = renderer.create(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const homePage = component.toJSON();
+    expect(homePage).toMatchSnapshot();
+
+    // const testInstance = component.root;
+    //
+    // expect(testInstance.findByType(Transactions)).toMatchSnapshot();
+
+    // const root = document.createElement("div");
+    // document.body.appendChild(root);
+    //
+    // render(
+    //   <MemoryRouter initialEntries={["/categories"]}>
+    //     <App />
+    //   </MemoryRouter>,
+    //   root
+    // );
+    //
+    // act(() => {
+    //   const goHomeLink = document.querySelector(".site-navigation")[0];
+    //   goHomeLink.dispatchEvent(new MouseEvent("click"));
+    // });
+    //
+    // expect(document.body.textContent).toBe("Transactions");
+  });
+
+  it("should render Categories page WHEN route is /categories", () => {
+    //todo implement the test
+    expect(true).toBe(false);
+  });
 });
