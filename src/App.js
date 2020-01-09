94b3616c9da942218b05065277a427496b8a5e14
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2020-01-09 21:56:33 +0300

    add tests for Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import Transactions from "./components/transactions/transactions";
+import Categories from "./components/categories/categories";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import { Route, Switch } from "react-router-dom";
+import { PATHNAMES } from "./constants/constants";
 
 import "./App.scss";
 
@@ -16,15 +18,8 @@ class App extends Component {
       <>
         <Header />
         <Switch>
-          <Route path="/" exact component={Transactions} />
-          <Route
-            path="/categories"
-            render={() => (
-              <div id="categoriesPage">
-                Categories page is under construction11
-              </div>
-            )}
-          />
+          <Route path={PATHNAMES.HOME} exact component={Transactions} />
+          <Route path={PATHNAMES.CATEGORIES} component={Categories} />
         </Switch>
         <Footer />
       </>
--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,12 @@ import { act } from "react-dom/test-utils";
 import { MemoryRouter } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
 import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 import createStore from "./store/create-store";
 import App from "./App";
+import Transactions from "./components/transactions/transactions";
+import Categories from "./components/categories/categories";
+import { PATHNAMES } from "./constants/constants";
 
 const appJsx = (
   <ReduxProvider store={createStore()}>
@@ -16,7 +20,10 @@ const appJsx = (
 );
 
 describe("App", () => {
+  let component;
+
   beforeEach(() => {
+    component = shallow(<App />);
     const root = document.createElement("div");
     document.body.appendChild(root);
     render(appJsx, root);
@@ -49,5 +56,61 @@ describe("App", () => {
 
     const appComponent = renderer.create(appJsx).toJSON();
     expect(appComponent).toMatchSnapshot();
+  });
+
+  describe("Switch", () => {
+    let pageSwitch;
+    let pageRoutes;
+
+    beforeEach(() => {
+      pageSwitch = component.find("Switch");
+      pageRoutes = pageSwitch.find("Route");
+    });
+
+    it("should render a Switch", () => {
+      expect(pageSwitch).toHaveLength(1);
+    });
+
+    it("should render 2 routes", () => {
+      expect(pageRoutes).toHaveLength(2);
+    });
+
+    describe("Transactions Route", () => {
+      let transactionsRoute;
+
+      beforeEach(() => {
+        transactionsRoute = pageRoutes.find({ component: Transactions });
+      });
+
+      it("should render route for Transactions component", () => {
+        expect(transactionsRoute).toHaveLength(1);
+      });
+
+      it("should have correct path", () => {
+        expect(transactionsRoute.prop("path")).toEqual(PATHNAMES.HOME);
+      });
+
+      it("should have 'exact' prop passed", () => {
+        expect(transactionsRoute.prop("exact")).toBeTruthy();
+      });
+    });
+
+    describe("Categories Route", () => {
+      let categoriesRoute;
+
+      beforeEach(() => {
+        categoriesRoute = pageRoutes.find({
+          component: Categories
+        });
+      });
+
+      it("should render route for Categories component", () => {
+        expect(categoriesRoute).toHaveLength(1);
+      });
+
+      it("should have correct path", () => {
+        expect(categoriesRoute.prop("path")).toEqual(PATHNAMES.CATEGORIES);
+      });
+    });
   });
 });
