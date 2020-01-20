94e6029d70daca385c7e72cf8f6f17db9f28b4c0
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2020-01-20 22:50:20 +0300

    WIP add styles to Add Modal

--- src/components/add-modal/add-modal.js
+++ src/components/add-modal/add-modal.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactModal from "react-modal";
 import PropTypes from "prop-types";
 
+import "./add-modal.scss";
+
 const AddModal = ({ isOpen, onClose }) => {
   return (
-    <ReactModal isOpen={isOpen} onRequestClose={onClose}>
-      AddReactModal
+    <ReactModal className="add-modal" isOpen={isOpen} onRequestClose={onClose}>
+      Transaction
     </ReactModal>
   );
 };
