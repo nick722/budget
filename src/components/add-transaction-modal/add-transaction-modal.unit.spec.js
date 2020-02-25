c363869962059c1a9e7cc2b71b40a066fc49c914
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2020-02-25 21:44:13 +0300

    fix tests for add-transaction-modal.js

--- src/components/add-transaction-modal/add-transaction-modal.unit.spec.js
+++ src/components/add-transaction-modal/add-transaction-modal.unit.spec.js
@@ -1,15 +1,42 @@
 import React from "react";
 import { shallow } from "enzyme";
 import AddTransactionModal from "./add-transaction-modal";
+import AddTransactionForm from "../add-transaction-form/add-transaction-form";
 
 const defaultProps = {
-  isOpen: false,
-  onClose: jest.fn()
+  addTransactionFormSubmitHandler: jest.fn(),
+  handleCloseAddModal: jest.fn(),
+  isOpen: false
 };
 
 describe("AddTransactionModal component", () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<AddTransactionModal {...defaultProps} />);
+  });
+
   it("should render into the document", () => {
-    const component = shallow(<AddTransactionModal {...defaultProps} />);
     expect(component).toHaveLength(1);
+  });
+
+  describe("AddTransactionForm", () => {
+    let addTransactionForm;
+
+    beforeEach(() => {
+      addTransactionForm = component.find(AddTransactionForm);
+    });
+
+    it("should render into the document", () => {
+      expect(addTransactionForm).toHaveLength(1);
+    });
+
+    it("should have expected props", () => {
+      expect(addTransactionForm.props()).toMatchObject({
+        addTransactionFormSubmitHandler:
+          defaultProps.addTransactionFormSubmitHandler,
+        handleCloseAddModal: defaultProps.handleCloseAddModal
+      });
+    });
   });
 });
