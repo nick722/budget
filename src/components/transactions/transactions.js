600e1da69deeaf32abb3419082196a528c7b4cc0
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2020-03-22 18:26:51 +0300

    use hooks in Transactions component

--- src/components/transactions/transactions.js
+++ src/components/transactions/transactions.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
@@ -17,68 +17,61 @@ import InstrumentsPane from "./instruments-pane/instruments-pane";
 
 import "./transactions.scss";
 
-export class Transactions extends Component {
-  state = {
-    addModalOpen: false
-  };
-
-  componentDidMount() {
-    this.props.fetchTransactions();
+const handleAddTransaction = (data, addTransaction, isPosting) => {
+  if (!isPosting) {
+    addTransaction(data);
   }
+};
 
-  componentDidUpdate(prevProps) {
-    if (
-      this.props.transactions.isPosting !== prevProps.transactions.isPosting
-    ) {
-      this.props.fetchTransactions();
-    }
-  }
+export const Transactions = ({
+  addTransaction,
+  isPosting,
+  fetchTransactions,
+  transactions
+}) => {
+  const [addModalOpen, setAddModalOpen] = useState(false);
 
-  handleOpenAddModal = () => {
-    this.setState({ addModalOpen: true });
-  };
+  useEffect(
+    () => {
+      fetchTransactions();
+    },
+    [transactions.isPosting]
+  );
 
-  handleCloseAddModal = () => {
-    this.setState({ addModalOpen: false });
-  };
-
-  handleAddTransaction = data => {
-    const { isPosting, addTransaction } = this.props;
-    if (!isPosting) {
-      addTransaction(data);
-      this.handleCloseAddModal();
-    }
-  };
-
-  render() {
-    const { addTransaction, transactions } = this.props;
-
-    return (
-      <div className="transactions">
-        <div className="nave-pane">
-          <div className="nave-pane__month-panel">
-            <button className="nave-pane__month-panel--arrow">
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </button>
-            <div className="nave-pane__month-panel--banner">October 2019</div>
-            <button className="nave-pane__month-panel--arrow">
-              <FontAwesomeIcon icon={faChevronRight} />
-            </button>
-          </div>
+  return (
+    <div className="transactions">
+      <div className="nave-pane">
+        <div className="nave-pane__month-panel">
+          <button className="nave-pane__month-panel--arrow">
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+          <div className="nave-pane__month-panel--banner">October 2019</div>
+          <button className="nave-pane__month-panel--arrow">
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
         </div>
-        <BalancePane />
-        <TransactionsList transactions={transactions} />
-        <InstrumentsPane handleOpenAddModal={this.handleOpenAddModal} />
-        <AddTransactionModal
-          addTransactionFormSubmitHandler={this.handleAddTransaction}
-          appElement={document.getElementById("root")}
-          isOpen={this.state.addModalOpen}
-          handleCloseAddModal={this.handleCloseAddModal}
-        />
       </div>
-    );
-  }
-}
+      <BalancePane />
+      <TransactionsList transactions={transactions} />
+      <InstrumentsPane
+        handleOpenAddModal={() => {
+          setAddModalOpen(true);
+        }}
+      />
+      <AddTransactionModal
+        addTransactionFormSubmitHandler={data => {
+          handleAddTransaction(data, addTransaction, isPosting);
+          setAddModalOpen(false);
+        }}
+        appElement={document.getElementById("root")}
+        isOpen={addModalOpen}
+        handleCloseAddModal={() => {
+          setAddModalOpen(false);
+        }}
+      />
+    </div>
+  );
+};
 
 Transactions.defaultProps = {
   transactions: []
