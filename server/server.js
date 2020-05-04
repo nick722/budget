57de591ca08fd7f654925e133f1399e41255b275
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2020-05-04 13:52:05 +0300

    create index router

--- server/server.js
+++ server/server.js
@@ -2,26 +2,13 @@ const express = require("express");
 const server = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const routes = require("./routes");
 // src / components / add - transaction - form / add - transaction - form.js;
-
-let transactions = [];
-transactions = require("./transactions");
 
 server.use(cors());
 server.use(bodyParser.json());
 
-server.get("/", (req, res) => {
-  res.send(transactions);
-});
-
-server.post("/", (req, res) => {
-  transactions.push({
-    description: req.body.description,
-    amount: req.body.amount,
-    id: Date.now()
-  });
-  res.sendStatus(200);
-});
+server.use("/", routes);
 
 server.use((req, res) => {
   res.status(404).send("<h1>404. Page not found</h1>");
